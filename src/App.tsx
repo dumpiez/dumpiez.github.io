f7bce9175e5ecc24bb3a1a65ce4a97a1d37bfc7b
dumpiez <nightlypie@gmail.com>
2026-07-19 02:22:07 +0700

    get basic header for the website done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
 export default function App() {
-  return <h1 className="text-4xl font-bold">hello</h1>;
+  return (
+    <div className="h-screen flex justify-center items-center flex-col">
+      <h1 className="text-4xl font-bold pb-4">hi! i am piezzz....</h1>
+      <p className="text-2xl font-medium text-balance text-center">
+        i am a self-taught developer{" "}
+        <a
+          href="#"
+          className="text-blue-300 hover:text-blue-400 no-underline transition-colors"
+        >
+          (and artist!)
+        </a>{" "}
+        currently in-the-works{" "}
+        <span className="text-yellow-400">
+          (i say this because im still very much a beginner)
+        </span>
+        , mainly focusing on web development, but expect game dev stuff from me
+        in the future..
+      </p>
+    </div>
+  );
 }
